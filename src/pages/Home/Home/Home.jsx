d5748a3d07f574735bd608e4d8cfff85ed7a4909
Abrarul Rhythm <[....email>
2025-11-16 01:42:02 +0600

    added banner slider

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
-        <div>
-            This is Home
-        </div>
+        <>
+            <div className='container'>
+                <Banner></Banner>
+            </div>
+        </>
     );
 };
 
