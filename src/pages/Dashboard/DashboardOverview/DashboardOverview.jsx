c32ef607ba678cdf434087746a9e66942e2c0702
Abrarul Rhythm <[....email>
2025-11-26 19:10:18 +0600

    setup tanstack query and fetch my parcels data

--- src/pages/Dashboard/DashboardOverview/DashboardOverview.jsx
+++ src/pages/Dashboard/DashboardOverview/DashboardOverview.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const DashboardOverview = () => {
     return (
         <div>
+            <title>Dashboard - Zap Shift</title>
             DashboardOverview
         </div>
     );
