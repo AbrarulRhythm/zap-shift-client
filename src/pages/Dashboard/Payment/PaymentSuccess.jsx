cf46afa53806478463ee4158ff0fc31e861119e8
Abrarul Rhythm <[....email>
2025-11-28 19:47:01 +0600

    added payment history

--- src/pages/Dashboard/Payment/PaymentSuccess.jsx
+++ src/pages/Dashboard/Payment/PaymentSuccess.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import paymentSuccessful from '../../../assets/payment-successful.png';
 import { useSearchParams } from 'react-router';
 import useAxiosSecure from '../../../hooks/useAxiosSecure';
@@ -6,6 +6,7 @@ import { toast } from 'react-toastify';
 
 const PaymentSuccess = () => {
     const [searchParams] = useSearchParams();
+    const [paymentInfo, setPaymentInfo] = useState({});
     const sessionId = searchParams.get('session_id');
     const axiosSecure = useAxiosSecure();
 
@@ -13,7 +14,12 @@ const PaymentSuccess = () => {
         if (sessionId) {
             axiosSecure.patch(`/payment-success?session_id=${sessionId}`)
                 .then(res => {
-                    if (res.data.modifiedCount) {
+                    console.log(res.data);
+                    setPaymentInfo({
+                        transactionId: res.data.transactionId,
+                        trackingId: res.data.trackingId
+                    })
+                    if (res.data.modifyParcel.modifiedCount) {
                         toast.success('Payment Successfully 🎉');
                     }
                 });
@@ -26,7 +32,9 @@ const PaymentSuccess = () => {
 
             <div className='py-10'>
                 <img src={paymentSuccessful} className='mx-auto mb-6 md:mb-8 w-28 lg:w-auto' alt="payment-successful" />
-                <h2 className='text-3xl md:text-4xl font-extrabold text-dark-12'>Payment Completed Successfully</h2>
+                <h2 className='text-3xl md:text-4xl font-extrabold text-dark-12 mb-3'>Payment Completed Successfully</h2>
+                <p>Your Transaction ID: {paymentInfo.transactionId}</p>
+                <p>Your Tracking ID: {paymentInfo.trackingId}</p>
             </div>
         </div>
     );
