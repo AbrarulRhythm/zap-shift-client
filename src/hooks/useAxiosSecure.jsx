bbe3aa28dbb3984069ca77b204367535f2ce4a4c
Abrarul Rhythm <[....email>
2025-11-29 23:44:43 +0600

    fixed token issue

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -16,7 +16,7 @@ const useAxiosSecure = () => {
     useEffect(() => {
         // Request Interceptor
         const reqInterceprot = axiosSecure.interceptors.request.use(config => {
-            config.headers.Authorization = `Bearer  ${user?.accessToken}`;
+            config.headers.Authorization = `Bearer ${user?.accessToken}`;
             return config;
         });
 
